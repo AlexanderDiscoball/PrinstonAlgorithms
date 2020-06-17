fc1dffc26ca9329ced041384c58d977bcbba7453
Sasha < a.hutornoy@mcc.vniiem.ru>
2020-06-17 11:21:12 +0300

    First commit

---
+++ Prinston/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import UnionFind.QuickFind.QuickFindUF;
+import UnionFind.UF;
+
+public class Main {
+    public static void main(String[] args) {
+        int N = 10;
+        UF uf = new QuickFindUF(N);
+
+    }
+}
---
+++ Prinston/src/main/java/UnionFind/UF.java
@@ -0,0 +1,6 @@
+package UnionFind;
+
+public interface UF {
+    boolean isConnected(int p, int q);
+    void union(int p, int q);
+}
